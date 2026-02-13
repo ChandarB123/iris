ce8e169cc883532d9a018832779974fcc44175eb
Chandar <chandar.barathi123@gmail.com>
2026-02-14 00:31:12 +0530

    typo fixed

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from hermes.io import read_json, write_json
 from hermes.paths import NEWS
 
 output_path = "_data/items.json"
-with open(output_path, "wb") as f:
+with open(output_path, "w") as f:
     f.write(read_json(NEWS))
