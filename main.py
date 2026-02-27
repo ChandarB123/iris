b7ed07c12461b473a97055e5aefaeadde110e7ee
Chandar <chandar.barathi123@gmail.com>
2026-02-27 08:49:14 +0530

    Add bonds data export to _data/bonds.json

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 from hermes.io import read_json, write_json
-from hermes.paths import NEWS
+from hermes.paths import NEWS, BONDS
 
 output_path = "_data/items.json"
 with open(output_path, "w") as f:
     json.dump(read_json(NEWS), f, indent=2)
+
+with open("_data/bonds.json", "w") as f1:
+    json.dump(read_json(BONDS), f1, indent=2)
